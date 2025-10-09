27ac4a3a978773d83f77a0ecc675b937df0f386d
thekevinhu <22kevinhu@gmail.com>
2025-10-09 19:30:03 -0400

    Add live site link for BubbledIn project in Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -38,6 +38,7 @@ const projects = [
         tech_img: [react_img, typescript_img, firebase_img],
         github: 'https://github.com/elenesturua/BubbledIn',
         category: "Web App",
+        liveSite: 'https://https://bubbled-in.vercel.app',
         featured: true
     },
     { 
